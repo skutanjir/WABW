751a2ded05a683208a239de650e105089a2867d1
unknown <sulistyofajarpratama@gmail.com>
2025-09-30 15:24:49 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-</head>
-<body>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
     <div class="container mt-5">
         <h1 class="mb-4">Daftar Pegawai</h1>
     <table border="1" cellpadding="5" cellspacing="0">
@@ -43,5 +40,4 @@
         </tbody>
     </table>
     </div>
-</body>
-</html>
+@endsection
